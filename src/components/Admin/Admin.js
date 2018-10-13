9066af7d948f107730d215a9c8cb3f54e8b03532
Manny Jo <jomansang@gmail.com>
2018-10-13 12:04:51 -0500

    add axios for connecting to the server and table on the DOM

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 class Admin extends Component {
+    state = {
+        feedbackList: [],
+    }
+    // get all the feedbacks from the server
+    getFeedback = () => {
+        axios({
+            method: 'GET',
+            url: '/feedback'
+        }).then(response => {
+            console.log(response.data);
+        }).catch(error => {
+            console.log('Error with getting feedback');
+        });
+    }
+
+    componentDidMount = () => {
+        this.getFeedback();
+    }
+
     render() {
         return (
             <div>
                 <h1>Feedback Results!</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Feeling</td>
+                            <td>Comprehension</td>
+                            <td>Support</td>
+                            <td>Comments</td>
+                            <td>Delete</td>
+                        </tr>
+                    </thead>
+                    <tbody>
 
+                    </tbody>
+                </table>
             </div>
         );
     }
