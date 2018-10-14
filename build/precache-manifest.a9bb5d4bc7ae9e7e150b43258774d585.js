5b857b9737dd55e99e4ab6d93d113aba3dd636f2
Manny Jo <jomansang@gmail.com>
2018-10-14 17:03:36 -0500

    test heroku

---
+++ build/precache-manifest.a9bb5d4bc7ae9e7e150b43258774d585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a4ab120b4eb7dad5f207",
+    "url": "/static/js/main.a4ab120b.chunk.js"
+  },
+  {
+    "revision": "8037154bc8052e73bbf6",
+    "url": "/static/js/1.8037154b.chunk.js"
+  },
+  {
+    "revision": "a4ab120b4eb7dad5f207",
+    "url": "/static/css/main.a86d464a.chunk.css"
+  },
+  {
+    "revision": "0f0a2f7062967ca55408aa7c5dae20d4",
+    "url": "/index.html"
+  }
+];
